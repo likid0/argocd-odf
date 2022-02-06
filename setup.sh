b0fcc4d51afcea8c8fcce361cdf126c9cd3b4543
Daniel Parkes <dparkes@redhat.com>
2022-02-06 21:45:34 +0100

    odf

--- setup.sh
+++ setup.sh
@@ -13,21 +13,33 @@ echo "Installing ArgoCD operator..."
 oc apply -f ./bootstrap/argocd-operator.yaml
 sleep 60
 
-# Wait time to install operators
+waitoperatorpod() {
+  sleep 10
+  oc get pods -n openshift-operators | grep ${1} | awk '{print "oc wait --for condition=Ready -n openshift-operators pod/" $1 " --timeout 300s"}' | sh
+  sleep 20
+}
+
+waitknativeserving() {
+  sleep 10
+  oc get pods -n knative-serving | grep ${1} | awk '{print "oc wait --for condition=Ready -n knative-serving pod/" $1 " --timeout 300s"}' | sh
+  sleep 20
+}
+
 echo "Waiting for Operators to be ready..."
-sleep 60
+waitoperatorpod gitops
 
 
-oc apply -f ./bootstrap
-
-
-#For argocd to allow deployment of cluster resources
+#oc apply -f ./bootstrap
+echo "Creating ArgoCD Server, project and Applications"
 oc project gitops-argocd
+helm template ./bootstrap/  --debug --namespace gitops-argocd  | oc apply -f -
 
-#oc create secret generic argocd-default-cluster-config --from-literal=clusterResources=true
+
+#Some fixes to be removed
+#For argocd to allow deployment of cluster resources outside of namespace
 oc create secret generic argocd-default-cluster-config --from-literal=namespaces=gitops-argocd --from-literal=server=https://kubernetes.default.svc --from-literal=config={"tlsClientConfig":{"insecure":false}} --from-literal=name=in-cluster --from-literal=clusterResources=true= --dry-run=client  -o yaml  | oc replace -f -
 
-#Just for testing, so we can deploy ODF in openshift-storage namespace
+#Not for final users, so we can deploy ODF in openshift-storage namespace
 oc adm policy add-cluster-role-to-user cluster-admin -z argocd-argocd-application-controller -n gitops-argocd
 
 # Add the namespaces where you have argocd deployed
