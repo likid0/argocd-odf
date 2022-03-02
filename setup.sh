9e4891c3fcffe3bbc395245720bce7d9545a759d
Daniel Parkes <dparkes@redhat.com>
2022-03-02 14:05:52 +0100

    fixed some issues with rbac and wait pods

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Setup Openshift Environment
 ##
 
-# Create Namespaces in order to deploy applications
+# Create Namespaces in order to deploy a new argocd instance
 echo "Creating namespaces..."
 oc new-project gitops-argocd
 
@@ -24,7 +24,7 @@ waitoperatorpod gitops
 
 
 #oc apply -f ./bootstrap
-echo "Creating ArgoCD Server, project and Applications"
+echo "Creating ArgoCD Server, project and Applications in gitops-argocd namespace"
 oc project gitops-argocd
 helm template ./bootstrap/  --debug --namespace gitops-argocd  | oc apply -f -
 
@@ -36,5 +36,10 @@ oc create secret generic argocd-default-cluster-config --from-literal=namespaces
 #Not for final users, so we can deploy ODF in openshift-storage namespace
 oc adm policy add-cluster-role-to-user cluster-admin -z argocd-argocd-application-controller -n gitops-argocd
 
-# Add the namespaces where you have argocd deployed
+#Important warning: Do not blindly elevate the permissions of application delivery instances to be cluster scoped. 
+#The instances you elevate should always be managed by an administrative team, not by application delivery teams.
+#Anyone with access to the namespace of a cluster-scoped instance can elevate their privileges on the cluster to become a cluster administrator themselves.
+#The next patch will make the Argo CD instances installed in the gitops-argocd become a cluster scoped instance.
+#Just as a reminder the per default openshift-gitops argocd instance is already cluster scoped.
 oc patch subscriptions.operators.coreos.com openshift-gitops-operator -n openshift-operators --patch-file bootstrap/patch/07_patch.yaml --type merge
+#oc create secret generic argocd-default-cluster-config --from-literal=namespaces=gitops-argocd --from-literal=server=https://kubernetes.default.svc --from-literal=config={"tlsClientConfig":{"insecure":false}} --from-literal=name=in-cluster --from-literal=clusterResources=true= --dry-run=client  -o yaml  | oc replace -f -
